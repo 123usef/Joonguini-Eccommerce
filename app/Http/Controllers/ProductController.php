1d3d8d00dada2e817f5bf7f10f6bea4060139e23
123usef <yousifm836@gmail>
2025-07-16 09:23:34 +0400

    dd product controller dd commented

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,7 +9,7 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::all();
-        dd($products);
+      //  dd($products);
         return view('Products.products', compact('products'));
     }
 }
